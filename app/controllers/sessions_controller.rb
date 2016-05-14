008aa84bbec0724f11a2877d5702e5d8b5b3e8dd
Jeff Cohen <cohen.jeff@gmail.com>
2016-05-13 19:33:09 -0500

    Finished with secure users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user
-      if user.password == params[:password]
+      if user.authenticate(params[:password])
         session["user_id"] = user.id
         redirect_to movies_url
         return
+      else
+        # Email is ok, but password was wrong
       end
+    else
+      # Email is unknown
     end
     redirect_to movies_url
   end
